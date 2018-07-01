ff11418df53bddaebc6264301cd5bfb992ea064e
ksjr7 <kommurujaishankarreddy@gmail.com>
2018-07-01 17:32:19 +0530

    important: Added actual definitions of module_init and module_exit

--- include/linux/module.h
+++ include/linux/module.h
@@ -165,6 +165,19 @@ extern void begin_fini(void);
 	{ return exitfn; }					\
 	void cleanup_module(void) __attribute__((alias(#exitfn)));
 #endif
+
+/* Each module must use one module_init(). */
+#define module_init(initfn)                                     \
+        static inline initcall_t __maybe_unused __inittest(void)                \
+        { return initfn; }                                      \
+        int init_module(void) __attribute__((alias(#initfn)));
+
+/* This is only required if you want to be unloadable. */
+#define module_exit(exitfn)                                     \
+        static inline exitcall_t __maybe_unused __exittest(void)                \
+        { return exitfn; }                                      \
+        void cleanup_module(void) __attribute__((alias(#exitfn)));
+
 #endif
 
 /* This means "can be init if no module support, otherwise module load
